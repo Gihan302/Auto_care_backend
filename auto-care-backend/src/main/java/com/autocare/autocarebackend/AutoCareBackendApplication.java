f111b44565db20ed549b9903b5db4dbe3c715710
Isuru35196 <isurugayantha35196@gmail.com>
2025-08-12 09:15:14 +0530

    create-project
    create-project

---
+++ auto-care-backend/src/main/java/com/autocare/autocarebackend/AutoCareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.autocare.autocarebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoCareBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoCareBackendApplication.class, args);
+    }
+
+}
